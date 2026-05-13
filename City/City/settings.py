65af8f6d6decad2c569fc0db10cb47ee66a7f27c
vinayakck2002 <vinayakck317@gmail.com>
2026-05-14 00:02:58 +0530

    Fixed Render email port to 465 SSL

--- City/City/settings.py
+++ City/City/settings.py
@@ -123,8 +123,9 @@ STATIC_URL = 'static/'
 #Email configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
