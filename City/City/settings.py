43c372751d869d4b9ea07727691031a29c8c9911
vinayakck2002 <vinayakck317@gmail.com>
2026-05-13 16:13:35 +0530

    Added allowed hosts for Render

--- City/City/settings.py
+++ City/City/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['offzon.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
